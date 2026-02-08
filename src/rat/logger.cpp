ffd05213dcf848810041519f573c9851676b262d
Spheya <tamara@spheya.com>
2026-02-08 18:10:47 +0100

    logger

---
+++ src/rat/logger.cpp
@@ -0,0 +1,43 @@
+#include "logger.hpp"
+
+#include <iostream>
+
+namespace rat {
+	static std::function<void(std::string_view, std::source_location, LogLevel)> s_MessageCallback; // NOLINT
+
+	void setMessageCallback(std::function<void(std::string_view, std::source_location, LogLevel)> callback) {
+		s_MessageCallback = callback;
+	}
+
+	void message(std::string_view message, std::source_location location, LogLevel level) {
+		if(s_MessageCallback) {
+			s_MessageCallback(message, location, level);
+		} else {
+			std::string_view fileName = location.file_name();
+			size_t lastSlash = fileName.find_last_of("\\/");
+			if(lastSlash != std::string_view::npos && lastSlash != fileName.size() - 1) fileName = fileName.substr(lastSlash + 1);
+
+			std::string msg;
+			size_t len = message.length();
+			switch(level) {
+			case LogLevel::Warn:
+				msg = "\033[1;33mWarn > \033[0m" + std::string(message);
+				len += 7;
+				break;
+			case LogLevel::Error:
+				msg = "\033[1;31mError > \033[0m" + std::string(message);
+				len += 8;
+				break;
+			default:
+				msg = "> " + std::string(message);
+				len += 2;
+				break;
+			}
+
+			if(len < 64) { msg.resize(64 + (msg.length() - len), ' '); }
+
+			std::cout << msg << "\033[1;30m (" << fileName << ":" << location.line() << ")\033[0m" << std::endl;
+		}
+	}
+
+} // namespace rat
---
+++ src/rat/logger.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <concepts>
+#include <format>
+#include <functional>
+#include <source_location>
+#include <string_view>
+#include <type_traits>
+
+namespace rat {
+
+	enum class LogLevel { None = 0, Error = 1, Warn = 2, Info = 3, Trace = 4 };
+
+	template<typename... Args>
+	struct LogMessageImpl {
+		template<typename T>
+		    requires std::convertible_to<const T&, std::basic_string_view<char>>
+		consteval LogMessageImpl(T const& message, std::source_location location = std::source_location::current()) :
+		    message(message), location(location) {}
+
+		std::format_string<Args...> message;
+		std::source_location location;
+	};
+
+	template<typename... Args>
+	using LogMessage = LogMessageImpl<std::type_identity_t<Args>...>;
+
+	void setMessageCallback(std::function<void(std::string_view, std::source_location, LogLevel)> callback);
+	void message(std::string_view message, std::source_location location, LogLevel level);
+
+#ifndef RAT_STRIP_LOGGER
+
+	template<typename... Args>
+	inline void log(LogMessage<Args...> message, Args&&... args) {
+		rat::message(std::format(message.message, std::forward<Args&&>(args)...), message.location, LogLevel::Trace);
+	}
+
+	template<typename... Args>
+	inline void info(LogMessage<Args...> message, Args&&... args) {
+		rat::message(std::format(message.message, std::forward<Args&&>(args)...), message.location, LogLevel::Info);
+	}
+
+	template<typename... Args>
+	inline void warn(LogMessage<Args...> message, Args&&... args) {
+		rat::message(std::format(message.message, std::forward<Args&&>(args)...), message.location, LogLevel::Warn);
+	}
+
+	template<typename... Args>
+	inline void error(LogMessage<Args...> message, Args&&... args) {
+		rat::message(std::format(message.message, std::forward<Args&&>(args)...), message.location, LogLevel::Error);
+	}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void log(const T& value, std::source_location location = std::source_location::current()) {
+		message(std::format("{}", value), location, LogLevel::Trace);
+	}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void info(const T& value, std::source_location location = std::source_location::current()) {
+		message(std::format("{}", value), location, LogLevel::Info);
+	}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void warn(const T& value, std::source_location location = std::source_location::current()) {
+		message(std::format("{}", value), location, LogLevel::Warn);
+	}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void error(const T& value, std::source_location location = std::source_location::current()) {
+		message(std::format("{}", value), location, LogLevel::Error);
+	}
+
+#else
+	template<typename... Args>
+	inline void log([[maybe_unused]] LogMessage<Args...> message, [[maybe_unused]] Args&&... args) {}
+
+	template<typename... Args>
+	inline void info([[maybe_unused]] LogMessage<Args...> message, [[maybe_unused]] Args&&... args) {}
+
+	template<typename... Args>
+	inline void warn([[maybe_unused]] LogMessage<Args...> message, [[maybe_unused]] Args&&... args) {}
+
+	template<typename... Args>
+	inline void error([[maybe_unused]] LogMessage<Args...> message, [[maybe_unused]] Args&&... args) {}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void log([[maybe_unused]] const T& value, [[maybe_unused]] std::source_location location = std::source_location::current()) {}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void info([[maybe_unused]] const T& value, [[maybe_unused]] std::source_location location = std::source_location::current()) {}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void warn([[maybe_unused]] const T& value, [[maybe_unused]] std::source_location location = std::source_location::current()) {}
+
+	template<typename T>
+	    requires(!std::convertible_to<const T&, std::basic_string_view<char>>)
+	inline void error([[maybe_unused]] const T& value, [[maybe_unused]] std::source_location location = std::source_location::current()) {}
+#endif
+
+} // namespace rat
