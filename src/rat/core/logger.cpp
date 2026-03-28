21cdc50cc1fdedd481f31d6e0ec20256bcdf03e4
Spheya <33225496+Spheya@users.noreply.github.com>
2026-03-28 04:19:54 +0100

    remove info log level

--- src/rat/core/logger.cpp
+++ src/rat/core/logger.cpp
@@ -6,7 +6,6 @@ namespace rat {
 
 	void message(std::string_view message, LogLevel level) {
 		switch(level) {
-		case LogLevel::Info: std::cout << "\033[1;32mInfo > \033[0m"; break;
 		case LogLevel::Warn: std::cout << "\033[1;33mWarn > \033[0m"; break;
 		case LogLevel::Error: std::cout << "\033[1;31mError > \033[0m"; break;
 		default: std::cout << "> "; break;
