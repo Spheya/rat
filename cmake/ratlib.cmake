fef1d9e35a4dcfb2405d511c4ae200099386a2a9
Spheya <33225496+Spheya@users.noreply.github.com>
2026-04-06 03:27:23 +0200

    attempt to get a proper error

--- cmake/ratlib.cmake
+++ cmake/ratlib.cmake
@@ -20,9 +20,6 @@ function(init_rat_target name)
       -Wno-c++20-compat-pedantic
       -Wno-c99-compat
       -Wno-unsafe-buffer-usage
-      -fdiagnostics-show-template-tree
-      -fdiagnostics-show-option
-      -fdiagnostics-show-category=name
       -fvisibility=hidden
     )
   endif()
