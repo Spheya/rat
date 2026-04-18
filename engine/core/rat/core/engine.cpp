b46e3bf305e60021bf56b14ec9b73c6fe44e04fb
Spheya <tamara@spheya.com>
2026-04-18 14:33:18 +0200

    glfw shouldn't load opengl

--- engine/core/rat/core/engine.cpp
+++ engine/core/rat/core/engine.cpp
@@ -2,6 +2,7 @@
 
 #include <cassert>
 
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 #include "editorcontext.hpp"
--- rat/main.cpp
+++ rat/main.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <rat/core/editorcontext.hpp>
 
